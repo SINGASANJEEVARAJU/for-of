f5baeb6a897cee87fe54c165aa62264fddc68a70
SINGA SANJEEVARAJU <sanjeevaraj123@gmail.com>
2024-10-27 20:16:11 +0530

    files are added

---
+++ day6.js
@@ -0,0 +1,46 @@
+switch("sunday"){
+    case "monday":
+    console.log("going to siva temple");
+    break;
+    case "tuesday":
+    console.log("going to office work");
+    break;
+    case "wednesday":
+    console.log("going to beach");
+    break;
+    case "thursday":
+    console.log("watching movie");
+    break;
+    case "friday":
+    console.log("meeting with clients")
+    break;
+    case "saturday":
+    console.log("weekend enjoy with frds")
+    break;
+    case "sunday":
+    console.log("make the biryani and eat with my family")
+    break;
+    default:
+    console.log("above options are not working properly");
+    
+}
+
+var a=[1,2,3,4,5];
+for (var string of a){
+    console.log(string)
+}
+
+
+
+var b="sucharitha"
+for( var string of b){
+    console.log(string)
+}
+
+
+var b="sanjeevaraju";
+var result=" "
+for( var string of b){
+    result=result+string
+    console.log(result)
+}
